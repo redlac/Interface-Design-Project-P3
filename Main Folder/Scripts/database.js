9749dcfcf5cd757caf662848ec98ba58cba02287
Derplar <48773777+Derplar@users.noreply.github.com>
2019-03-22 10:45:11 -0600

    Added Main Folder
    This is a lot cleaner and give the idea of how to organize things.  You can work in your individual areas, but at the end of the day it should work with everything in here.

---
+++ Main Folder/Scripts/database.js
@@ -0,0 +1,71 @@
+const regularDatabase = 
+[
+	{
+		'itemID' : 1000,
+		'itemName' : "Tomato"
+		'description' : "Testeroni",
+		'stock' : 78,
+		'quantity' : 0,
+		'picUrl' : "../images/placeholder.png"
+	},
+	{
+		'itemID' : 1001
+		'itemName' : "Banana"
+		'description' : "Testeroni",
+		'stock' : 1573,
+		'quantity' : 0,
+		'picUrl' : "../images/placeholder.png"
+	},
+	{
+		'itemID' : 1002
+		'itemName' : "Cucumber"
+		'description' : "Testeroni",
+		'stock' : 15,
+		'quantity' : 0,
+		'picUrl' : "../images/placeholder.png"
+	}
+]
+
+const specialDatabase =
+[
+	{
+		'itemID' : 6327,
+		'itemName' : "Ash Brick"
+		'description' : "Testeroni",
+		'stock' : 26,
+		'quantity' : 0,
+		'picUrl' : "../images/placeholder.png"
+	},
+	{
+		'itemID' : 6338
+		'itemName' : "Ash Dust"
+		'description' : "Testeroni",
+		'stock' : 5,
+		'quantity' : 0,
+		'picUrl' : "../images/placeholder.png"
+	}
+]
+
+const historyDatabase =
+[
+	{
+		'orderID' : 213314,
+		'numItems' : 47,
+		'dateShipped' : "",
+		'dateReceived' : "",
+		'status' : "",
+		'orderList' : new Array[]
+	}
+]
+
+const autoOrderDatabase =
+[
+	{
+		'itemID' : 24677,
+		'itemName' : "Mario Mushroom"
+		'description' : "Makes you feel high, maaaaan.",
+		'stock' : 69,
+		'quantity' : 0,
+		'picUrl' : "../images/placeholder.png"
+	}
+]
