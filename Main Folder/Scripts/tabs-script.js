c06f93fda32c92f8bef6db309fffb05c22817c27
Calder <Calder.Trombley@edu.sait.ca>
2019-03-26 19:01:15 -0600

    Update TODOs

--- Main Folder/Scripts/tabs-script.js
+++ Main Folder/Scripts/tabs-script.js
@@ -14,21 +14,21 @@ function openScreen(evt, screenName) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
+  // Show the current tab, and add an "active" class to the button that opened the tab
   switch (screenName){
     case "tabs-Regular":
-      // Show the current tab, and add an "active" class to the button that opened the tab
       document.getElementById(screenName).innerHTML = '<object type="text/html" data="regular_tab.html" width="100%" height="100%" style="overflow: hidden;"></object>';
       break;
     case "tabs-Special":
-      // Show the current tab, and add an "active" class to the button that opened the tab
+      /* TODO: change page url */ 
       document.getElementById(screenName).innerHTML = '<object type="text/html" data="special_tab.html" width="100%" height="100%" style="overflow: hidden;"></object>';
       break;
     case "tabs-History":
-      // Show the current tab, and add an "active" class to the button that opened the tab
+      /* TODO: change page url */ 
       document.getElementById(screenName).innerHTML = '<object type="text/html" data="history_tab.html" width="100%" height="100%" style="overflow: hidden;"></object>';
       break;
     case "tabs-OrderOptions":
-      // Show the current tab, and add an "active" class to the button that opened the tab
+      /* TODO: change page url */ 
       document.getElementById(screenName).innerHTML = '<object type="text/html" data="order_options_tab.html" width="100%" height="100%" style="overflow: hidden;"></object>';
       break;
   }
