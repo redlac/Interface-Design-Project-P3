8ff215fb1216e2c5c27743022413d54c7c07291e
Calder <Calder.Trombley@edu.sait.ca>
2019-03-27 09:03:03 -0600

    Add quantity update on click

--- Main Folder/Scripts/regular-js.js
+++ Main Folder/Scripts/regular-js.js
@@ -7,12 +7,16 @@ function regularGridMinusQty(itemNumber) {
         itemQty -= 1;
         document.getElementById("regular-GridQty" + itemNumber).value = itemQty;
     }
+    regularDatabase[itemNumber-1].quantity = itemQty;
+    console.log(regularDatabase);
 }
 
 function regularGridPlusQty(itemNumber) {
     itemQty = Number(document.getElementById("regular-GridQty" + itemNumber).value);
     itemQty += 1;
     document.getElementById("regular-GridQty" + itemNumber).value = itemQty;
+    regularDatabase[itemNumber-1].quantity = itemQty;
+    console.log(regularDatabase);
 }
 
 function regularListMinusQty(itemNumber) {
@@ -21,12 +25,16 @@ function regularListMinusQty(itemNumber) {
         itemQty -= 1;
         document.getElementById("regular-ListQty" + itemNumber).value = itemQty;
     }
+    regularDatabase[itemNumber-1].quantity= itemQty;
+    console.log(regularDatabase);
 }
 
 function regularListPlusQty(itemNumber) {
     itemQty = Number(document.getElementById("regular-ListQty" + itemNumber).value);
     itemQty += 1;
     document.getElementById("regular-ListQty" + itemNumber).value = itemQty;
+    regularDatabase[itemNumber-1].quantity = itemQty;
+    console.log(regularDatabase);
 }
 
 /**
@@ -44,22 +52,34 @@ document.getElementById("regular-view").onchange = function () {
         document.getElementById("regular-itemGrid").style.display = "none";
     }
 }
-console.log(regularDatabase);
-document.getElementById("regular-item-desc1").innerHTML = "<span>" + regularDatabase[0].itemName + "</span>"
+
+document.getElementById("regular-item-desc1").innerHTML = "<h4>" + regularDatabase[0].itemName + "</h4>"
                                                         + "<p>Stock: " + regularDatabase[0].stock + "</p>"
                                                         + "<p>" + regularDatabase[0].description + "</p>";
 
-document.getElementById("regular-item-desc2").innerHTML = "<span>" + regularDatabase[1].itemName + "</span>"
+document.getElementById("regular-item-desc2").innerHTML = "<h4>" + regularDatabase[1].itemName + "</h4>"
                                                         + "<p>Stock: " + regularDatabase[1].stock + "</p>"
                                                         + "<p>" + regularDatabase[1].description + "</p>";
 
-document.getElementById("regular-item-desc3").innerHTML = "<span>" + regularDatabase[2].itemName + "</span>"
+document.getElementById("regular-item-desc3").innerHTML = "<h4>" + regularDatabase[2].itemName + "</h4>"
                                                         + "<p>Stock: " + regularDatabase[2].stock + "</p>"
                                                         + "<p>" + regularDatabase[2].description + "</p>";
 
-document.getElementById("regular-grid-item-desc1").innerHTML = "<span>" + regularDatabase[0].itemName + "</span>"; 
-document.getElementById("regular-grid-item-desc2").innerHTML = "<span>" + regularDatabase[1].itemName + "</span>"; 
-document.getElementById("regular-grid-item-desc3").innerHTML = "<span>" + regularDatabase[2].itemName + "</span>"; 
-document.getElementById("regular-grid-item-desc4").innerHTML = "<span>" + regularDatabase[3].itemName + "</span>"; 
-document.getElementById("regular-grid-item-desc5").innerHTML = "<span>" + regularDatabase[4].itemName + "</span>"; 
-document.getElementById("regular-grid-item-desc6").innerHTML = "<span>" + regularDatabase[5].itemName + "</span>"; 
+document.getElementById("regular-item-desc4").innerHTML = "<h4>" + regularDatabase[3].itemName + "</h4>"
+                                                        + "<p>Stock: " + regularDatabase[3].stock + "</p>"
+                                                        + "<p>" + regularDatabase[3].description + "</p>";
+
+document.getElementById("regular-item-desc5").innerHTML = "<h4>" + regularDatabase[4].itemName + "</h4>"
+                                                        + "<p>Stock: " + regularDatabase[4].stock + "</p>"
+                                                        + "<p>" + regularDatabase[4].description + "</p>";
+
+document.getElementById("regular-item-desc6").innerHTML = "<h4>" + regularDatabase[5].itemName + "</h4>"
+                                                        + "<p>Stock: " + regularDatabase[5].stock + "</p>"
+                                                        + "<p>" + regularDatabase[5].description + "</p>";
+
+document.getElementById("regular-grid-item-desc1").innerHTML = "<h4>" + regularDatabase[0].itemName + "</h4>"; 
+document.getElementById("regular-grid-item-desc2").innerHTML = "<h4>" + regularDatabase[1].itemName + "</h4>"; 
+document.getElementById("regular-grid-item-desc3").innerHTML = "<h4>" + regularDatabase[2].itemName + "</h4>"; 
+document.getElementById("regular-grid-item-desc4").innerHTML = "<h4>" + regularDatabase[3].itemName + "</h4>"; 
+document.getElementById("regular-grid-item-desc5").innerHTML = "<h4>" + regularDatabase[4].itemName + "</h4>"; 
+document.getElementById("regular-grid-item-desc6").innerHTML = "<h4>" + regularDatabase[5].itemName + "</h4>"; 
