e3043cd42e81ebfa61c006c7c38e16ea84ce83ed
redlac <Calder.Trombley@edu.sait.ca>
2019-03-29 15:01:40 -0600

    add special tab data

--- Main Folder/Scripts/special-tab.js
+++ Main Folder/Scripts/special-tab.js
@@ -26,20 +26,10 @@ function specialListPlusQty(itemNumber) {
     document.getElementById("special-ListQty" + itemNumber).value = itemQty;
 }
 
-const specialDatabase = [{
-        'itemID': 6327,
-        'itemName': "Ash Brick",
-        'description': "Weight:3kg<br>Height:2 inches<br>Length:8 inches",
-        'stock': 26,
-        'quantity': 0,
-        'picUrl': "images/AshBrick.jpg",
-        'reference':"https://www.justdial.com/Vijayawada/SRI-DURGA-Bhavani-Flyash-Bricks-MYLAVARAM-Ibrahimpatnam/0866PX866-X866-180117200512-Z4H6_BZDET/photos"
-    },
-];
 
 function populateStuff() {
     document.getElementById("searchOutput").className = "hi";
-    document.getElementById("special-item-image-one").setAttribute("src", specialDatabase[0].picUrl)
+    document.getElementById("special-item-image-one").setAttribute("src", specialDatabase[0].picUrl);
     document.getElementById("special-item-desc-one").innerHTML = "<h4>" + specialDatabase[0].itemName + "</h4>" +
         "<p>Stock: " + specialDatabase[0].stock + "</p>" +
         "<p>" + specialDatabase[0].description + "</p>";
