dda9a84e0e877a9ce9168203771c8e0756d51615
redlac <Calder.Trombley@edu.sait.ca>
2019-04-08 12:14:05 -0600

    update regular tab

--- Main Folder/Scripts/home.js
+++ Main Folder/Scripts/home.js
@@ -1,9 +1,11 @@
+document.getElementById("newOrderBtn").style.display = "none";
+
 function validateLogin() {
     if (document.getElementById("username").value == "Kody" && document.getElementById("password").value == "CMPS253") {
         document.getElementById("greeting").innerHTML = "Hello Kody!";
         document.getElementById("loginInfo").className = "hidden";
         document.getElementById("loginInfo").style.display = "none";
-
+        document.getElementById("newOrderBtn").style.display = "block";
     } else {
         alert("Login information incorrect!");
     }
