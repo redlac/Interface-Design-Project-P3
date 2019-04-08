66405ed4e8211b6b03efdbe38a1a118b5229a3a6
nphanSAIT <nguyenkhanhduy.phan@edu.sait.ca>
2019-04-08 17:42:15 -0600

    still working on it

--- Main Folder/Scripts/Optional_script.js
+++ Main Folder/Scripts/Optional_script.js
@@ -71,6 +71,7 @@ function enabling() {
 }
 
 function getData() {
+    console.log("value: " + document.getElementById("search_by_selection").value);
     if (document.getElementById("search_by_selection").value == "1") {
         item_num = document.getElementById("input_item").value;
         data_file = new database_array();
@@ -81,9 +82,96 @@ function getData() {
     {
         var text = document.getElementById("input_item").value;
         text = text.toLowerCase();
-        
+        console.log(text);
+        searchByName(text);
     }
 
+}
+
+function remove_table(l) 
+{
+    for (var i = 0; i < l.length; i++) {
+        document.getElementById("table_output").deleteRow(l);
+    } 
+}
+
+function searchByName(n) 
+{
+    const myTable = document.querySelector("#table_output");
+    const table_length = length;
+    remove_table(table_length);
+    console.log(myTable);
+    console.log(flag, table_length);
+    document.getElementById("div_output").style.display = "block";
+    var str = n.toLowerCase();
+    for(let obj of myData) 
+    {
+        var objN = obj.itemName;
+        objN = objN.toLowerCase();
+        console.log(objN, str);
+        if(objN.startsWith(str)) 
+        {
+            console("check true")
+            myIDs.push(obj.itemID);
+            var product_name = JSON.stringify(obj.itemName);
+            console.log(product_name);
+            var image = document.createElement("img");
+            image.setAttribute("src", obj.picUrl);
+
+            var input_number = document.createElement("input");
+            input_number.setAttribute("type", "text");
+            input_number.setAttribute("id", obj.itemName.toString());
+            input_number.setAttribute("class", "input_number_field");
+            input_number.setAttribute("value", "1");
+
+            var plus_button = document.createElement("button");
+            plus_button.innerHTML = "+";
+            plus_button.setAttribute("id", "plus_button");
+            plus_button.setAttribute("value", "+");
+            plus_button.setAttribute("onclick", `plus_quantity(${product_name});`);
+            var minus_button = document.createElement("button");
+            minus_button.innerHTML = "-";
+            minus_button.setAttribute("value", "s");
+            minus_button.setAttribute("onclick", `minus_quantity(${product_name});`);
+
+            // var update_button = document.createElement("button");
+            // update_button.innerHTML = "Update";
+            // update_button.setAttribute("onclick", `updating_item(${obj.itemID});`);
+            var remove_button = document.createElement("button");
+            remove_button.innerHTML = "Remove";
+            remove_button.setAttribute("onclick", `removing_item(${obj.itemID});`);
+            var add_button = document.createElement("button");
+            add_button.innerHTML = "Add";
+            add_button.setAttribute("onclick", `adding_item(${obj.itemID});`);
+            //Rows
+            var row = myTable.insertRow(table_length + 1);
+            row.setAttribute("id", "row_out_put");
+
+            var item_id = row.insertCell(0);
+            item_id.innerHTML = parseInt(obj.itemID).toString();
+
+            var item_name = row.insertCell(1);
+            item_name.append(image);
+
+            var item_description = row.insertCell(2);
+            item_description.innerHTML = obj.itemName + "\n<br>" + obj.description;
+
+            var item_stock = row.insertCell(3);
+            item_stock.innerHTML = parseInt(obj.stock).toString();
+
+            var item_quantity = row.insertCell(4);
+            item_quantity.append(minus_button);
+            item_quantity.append(input_number);
+            item_quantity.append(plus_button);
+
+            var update_remove = row.insertCell(5);
+            update_remove.append(add_button);
+            update_remove.append(remove_button);
+
+            //document.getElementById("output_result").innerHTML = parseInt(obj.itemID).toString() + " at Index: " + myData.indexOf(obj) + " and ranked at positon: " + (myData.indexOf(obj) + 1);
+            console.log(parseInt(obj.itemID).toString() + " at Index: " + myData.indexOf(obj) + " and ranked at positon: " + (myData.indexOf(obj) + 1));
+        }
+    }
 }
 
 function removing_item(id) {
